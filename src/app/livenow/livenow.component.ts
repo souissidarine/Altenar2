98584ca058a1a08e3a22427cd5957d99e0bc213a
unknown <souissidarine0@gmail.com>
2021-03-04 12:58:03 +0100

    betslip

--- src/app/livenow/livenow.component.ts
+++ src/app/livenow/livenow.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {APIService} from '../api.service'
+import { APIService } from '../api.service'
 
 @Component({
   selector: 'app-livenow',
@@ -9,47 +9,61 @@ import {APIService} from '../api.service'
 })
 export class LivenowComponent implements OnInit {
   active = 1;
-  football : any;
-  basketball : any ;
-  tennis : any;
-  lv10 : any
-  
+  football: any;
+  basketball: any;
+  tennis: any;
+  lv10: any
 
-  
-  constructor(private http:HttpClient , private api:APIService) { }
- 
+  displayTotal = false;
+  displayBut = false;
+  displayEq = false;
+  display1x2 = false
+  Menu = []
+  constructor(private http: HttpClient, private api: APIService) { }
+
 
 
   ngOnInit(): void {
-    
-     this.getbasketball();
-     this.gettennis();
-     this.get10live();
-  
+
+    this.getbasketball();
+    this.gettennis();
+    this.get10live();
+
   }
 
-get10live(){
-  this.api.live10ev().subscribe((lv10:any)=>{
-    this.lv10 = lv10.Result.Items[0].Events ;
-    console.log(this.lv10)
-  })
-}
+  get10live() {
+    this.api.live10ev().subscribe((lv10: any) => {
+      this.lv10 = lv10.Result.Items[0].Events;
+      console.log(this.lv10)
+    })
+  }
 
-  getfootball(){
-    this.api.football().subscribe((ft: any)=>{
+  getfootball() {
+    this.api.football().subscribe((ft: any) => {
       this.football = ft;
     })
   }
-  getbasketball(){
-    this.api.basketball().subscribe((sk: any)=>{
+  getbasketball() {
+    this.api.basketball().subscribe((sk: any) => {
       this.basketball = sk;
     })
   }
-  gettennis(){
-    this.api.tennis().subscribe((tn: any)=>{
+  gettennis() {
+    this.api.tennis().subscribe((tn: any) => {
       this.tennis = tn;
     })
   }
 
-
+  DisplayTotal() {
+    this.displayTotal = !this.displayTotal
+  }
+  DisplayBut() {
+    this.displayBut = !this.displayBut
+  }
+  DisplayEq() {
+    this.displayEq = !this.displayEq
+  }
+  Display1x2() {
+    this.display1x2 = !this.display1x2
+  }
 }
