fc7e3e49c6fc68668f7e137e55fe8f871716b930
Med Ali Ezzeddine <47082236+xDaly@users.noreply.github.com>
2025-08-07 03:31:31 +0100

    Delete src directory

--- src/app/pipes/middle-dash-injector.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MiddleDashInjectorPipe } from './middle-dash-injector.pipe';
-
-describe('MiddleDashInjectorPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MiddleDashInjectorPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
